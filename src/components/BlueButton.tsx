efc5b1e9cf0f84ea694a400474fc2e33631b5776
NinaZurash <nino.zurashvili.nz@gmail.com>
2024-01-30 15:19:34 +0400

    update

--- src/components/BlueButton.tsx
+++ src/components/BlueButton.tsx
@@ -1,7 +1,3 @@
 export default function BlueButton() {
-  return (
-    <button className="bg-blue-900 text-amber-50 mx-auto flex">
-      BlueButton
-    </button>
-  );
+  return <button className="bg-blue-900 text-amber-50 mx-auto flex">0</button>;
 }
